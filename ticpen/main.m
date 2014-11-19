9d15277b135060d5b410b1ccd98a8a5a310b07cd
dlo_xlp <dlo_xlp@yahoo.co.jp>
2014-11-19 13:39:57 +0900

    Initial Commit

---
+++ ticpen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ticpen
+//
+//  Created by T.KADOWAKI on 2014/11/19.
+//  Copyright (c) 2014年 KUISAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
